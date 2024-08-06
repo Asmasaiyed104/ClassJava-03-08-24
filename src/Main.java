209edead71e084d898af661bd3cccb8b322ca0e6
Asma <saiyedas@shernet.sheridancollege.ca>
2024-08-06 14:56:46 -0400

    09-08-2024, 2:56PM added

--- src/Main.java
+++ src/Main.java
@@ -25,5 +25,13 @@ public class Main {
 
         System.out.println(employee.employee_id);
 
+
+        // student object
+        Student student = new Student();
+        // initialize with reference variable
+        student.student_id = 234565;
+        student.student_name="Asma";
+        student.student_email="az104@gmail.com";
+        System.out.println(student.student_name +  " " +student.student_id + " " +student.student_email );
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    // properties
+    int student_id;
+    String student_name;
+
+    String Student_address;
+
+    String student_email;
+
+
+    // method
+
+  public void  getGrade(){
+      System.out.println("student can get good marks after lots of study");
+  }
+
+}
